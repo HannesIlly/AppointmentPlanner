f90fc51107f87a9a35f12a3085179bc73d104102
Hannes Illy <Hannes.Illy@gmx.de>
2021-10-25 10:32:46 +0200

    Initial commit.

---
+++ src/main/kotlin/planner/appointmentPlanner/AppointmentPlannerApplication.kt
@@ -0,0 +1,11 @@
+package planner.appointmentPlanner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppointmentPlannerApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppointmentPlannerApplication>(*args)
+}
---
+++ src/test/kotlin/planner/appointmentPlanner/AppointmentPlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package planner.appointmentPlanner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppointmentPlannerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
